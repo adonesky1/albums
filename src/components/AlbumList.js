6fc3aa1b949217274f7251b6644c52735bc16631
Alex Donesky <adonesky@gmail.com>
2018-07-29 23:18:39 -0400

    added more sections to card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios'
 import AlbumDetail from './albumDetail'
 
@@ -27,9 +27,9 @@ class AlbumList extends Component {
 
     render(){
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>    
+            </ScrollView>    
      ) ; 
     }
 };
--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,12 +1,56 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import Card from './Card';
+import CardSection from './CardSection'
 
-export default AlbumDetail = (props) => {
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+     thumbnailStyle: {
+         height: 50, 
+         width: 50
+     },
+      thumbnailContainerStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginLeft: 10,
+          marginRight: 10,
+      },
+      headerTextStyle: {
+        fontSize: 18
+      },
+      imageStyle: {
+          height: 300,
+          flex: 1,
+          width: null
+      }
+}
+
+export default AlbumDetail = ( {album} ) => {
+    const { headerContentStyle, thumbnailStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles
+    const { title, artist, thumbnail_image, image } = album
 
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>        
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                    style={thumbnailStyle} 
+                    source={{ uri: thumbnail_image }}  
+                    />
+                </View>
+                <View style={headerContentStyle} >
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image}} />
+            </CardSection>
+
         </Card>
     )
 
